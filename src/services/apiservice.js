aa1417f1a724eb285462421bbef7dff2732d9b66
guuhfsouza <62706070+guuhfsouza@users.noreply.github.com>
2020-04-22 11:38:15 -0300

    alteração de api serviços

--- src/services/apiservice.js
+++ src/services/apiservice.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: "http://localhost:51522/api"//"https://gustavoapi.gear.host/api"//process.env.REACT_APP_APISERVICE_URL
+    baseURL: "https://gustavoapi.gear.host/api"//process.env.REACT_APP_APISERVICE_URL
 });
 
 
